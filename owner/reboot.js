1291cfdc5ff1f456b04933e21b28aefc08d1bbb5
Rodrigo Couto <rodrigo.couto.santos@hotmail.com>
2021-05-02 20:47:32 -0300

    Update reboot.js

--- owner/reboot.js
+++ owner/reboot.js
@@ -1,18 +1,21 @@
+const db = require("quick.db")
+
 exports.run = async (client, message, args) => {
 
     if (message.author.id !== "451619591320371213") {
         message.delete().catch(err => { return })
         return message.inlineReply('⚠️ Este é um comando restrito.').then(msg => msg.delete({ timeout: 5000 }))
     }
+    
+    let ServidoresAtivados = db.fetch(`globalchat_${message.guild.id}`)
+    if (message.channel.id === ServidoresAtivados) {
 
-    client.guilds.cache.forEach(guild => {
-
-        let CanaisValidos = guild.channels.cache.find(ch => ch.name === "naya-global-chat")
-
-        if (!CanaisValidos) return
-
-        return CanaisValidos.send('<a:carregando:836101628083437608> Rebooting...')
-    })
+        client.guilds.cache.forEach(Canal => {
+            try {
+                client.channels.cache.get(db.fetch(`globalchat_${Canal.id}`)).send('<a:carregando:836101628083437608> Reiniciando...')
+            } catch (e) { return }
+        })
+    }
 
     client.user.setActivity(`Rebooting...`, { type: "WATCHING" })
     return message.inlineReply('<a:carregando:836101628083437608> Rebooting...')
